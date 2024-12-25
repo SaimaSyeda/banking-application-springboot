d01c061e0fb296d9340579d270d63bc61576ea9f
SaimaSyeda <158795082+SaimaSyeda@users.noreply.github.com>
2024-12-25 13:50:27 +0530

    Updated BankingAppApplication.java

--- banking-app/banking-app/src/main/java/net/javaguides/banking/BankingAppApplication.java
+++ banking-app/banking-app/src/main/java/net/javaguides/banking/BankingAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BankingAppApplication {
 
+	//This is the main function
 	public static void main(String[] args) {
 		SpringApplication.run(BankingAppApplication.class, args);
 	}
